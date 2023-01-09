fa699568e4f1112d18ee86c52be202abe5dab27b
harikoyyani <73346965+harikoyyani@users.noreply.github.com>
2023-01-10 00:07:38 +0530

    Add files via upload

---
+++ Week2 (1).py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <h1> My Jupyter Notebook on IBM Watson Studio</h1>
+
+# <strong>K HARIPRIYA</strong><br>
+# Data Analyst
+# 
+# 
+
+# <em> I like data science because it helps to solve numerous problems using statistical methods and historical data</em>
+# 
+
+# <h3> My code displays sum of two numbers</h3>
+
+# In[1]:
+
+
+x= 5 + 3 
+
+
+# In[2]:
+
+
+x
+
+
+# Things I love to eat
+# <hr>
+# <ol>
+#     <li> Briyani
+#     <li> Noodles
+#     <li> Cake
+# </ol>
+# <hr>
+# Places I like to visit
+# <ul>
+#     <li> Swiss
+#     <li> Paris
+# </ul>
+#     
+
+# In[ ]:
+
+
+
+
